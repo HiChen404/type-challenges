759c9fc452bb634c377527aaefb4a8a45f61cdee
Chen <codingme@icloud.com>
2022-05-06 12:37:18 +0800

    feat: 3057

--- 43-easy-exclude/template.ts
+++ 43-easy-exclude/template.ts
@@ -1,9 +1,1 @@
-// type MyAwaited<T> = T extends Promise<infer X> ? X : T
-// type MyAwaited<T> = T extends Promise<infer X> ? MyAwaited<X> : T
-// type MyAwaited<T extends Promise<unknown>> = T extends Promise<infer X> ? MyAwaited<X> : T
-
-type MyAwaited<T extends Promise<unknown>> = T extends Promise<infer X>
-  ? X extends Promise<unknown>
-    ? MyAwaited<X>
-    : X
-  : T
+type MyExclude<T, U> = T extends U ? never : T
